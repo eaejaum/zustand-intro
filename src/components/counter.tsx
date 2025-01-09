61334bec7d49ec433f898c22c4525c565cd718d2
eaejaum <jpedroalmeida179@gmail.com>
2025-01-09 02:44:37 -0300

    testing state management with two differents components

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,22 +1,16 @@
-import { Counter } from "./components/counter";
-import { useCounter } from "./store/use-counter";
+import { useCounter } from "../store/use-counter";
 
-function App() {
+export function Counter() {
   const { count, increaseCount, decreaseCount, resetCount  } = useCounter();
 
   return (
     <>
-      <h1>Welcome to Zustand!</h1>
       <div>
-        <p>Quantidade: {count}</p>
+        <p>Quantidade 2: {count}</p>
         <button onClick={increaseCount}>Aumentar</button>
         <button onClick={decreaseCount}>Diminuir</button>
         <button onClick={resetCount}>Zerar</button>
       </div>
-      
-      <Counter /> 
     </>
   )
 }
-
-export default App
