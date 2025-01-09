f29f1a78e5ed78427c9dc7aca5a67f3b4206ac4f
eaejaum <jpedroalmeida179@gmail.com>
2025-01-09 02:49:30 -0300

    persist middleware

--- src/store/use-counter.tsx
+++ src/store/use-counter.tsx
@@ -1,15 +1,23 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface CounterStore {
-    count: number,
-    increaseCount: () => void;
-    decreaseCount: () => void;
-    resetCount: () => void;
+  count: number;
+  increaseCount: () => void;
+  decreaseCount: () => void;
+  resetCount: () => void;
 }
 
-export const useCounter = create<CounterStore>((set) => ({
-    count: 0,
-    increaseCount: () => set((state) => ({ count: state.count + 1})),
-    decreaseCount: () => set((state) => ({ count: state.count - 1 })),
-    resetCount: () => set(() => ({ count: 0 })),
-}))
+export const useCounter = create(
+  persist<CounterStore>(
+    (set) => ({
+      count: 0,
+      increaseCount: () => set((state) => ({ count: state.count + 1 })),
+      decreaseCount: () => set((state) => ({ count: state.count - 1 })),
+      resetCount: () => set(() => ({ count: 0 })),
+    }),
+    {
+        name: 'count-storage'
+    }
+  )
+);
